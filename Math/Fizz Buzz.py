591f5372092df7aa34540a9063af667db2580868
furkan aydogan <furkancan_98@hotmail.com>
2022-06-08 22:13:57 +0300

    fizzbuzz

---
+++ Math/Fizz Buzz.py
@@ -0,0 +1,21 @@
+#Given an integer n, return a string array answer (1-indexed) where:
+
+#answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+#answer[i] == "Fizz" if i is divisible by 3.
+#answer[i] == "Buzz" if i is divisible by 5.
+#answer[i] == i (as a string) if none of the above conditions are true.
+
+def fizzbuzz(n):
+    i = 1
+    while i <= n:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i = i+1
+
+fizzbuzz(int(input()))
